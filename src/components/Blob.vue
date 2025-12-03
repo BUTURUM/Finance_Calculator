16e5a014483865ae3c12f53137c2ecf1d4828a8c
BUTURUM <maxvolkov502@gmail.com>
2025-12-03 00:04:35 +0000

    fix: make add button smaller

--- src/components/Blob.vue
+++ src/components/Blob.vue
@@ -1,6 +1,6 @@
 <template>
   <button class="primary bg-box rounded-3xl p-3 fixed bottom-0 right-0 mb-5 mr-5">
-    <Plus class="size-14 fill-high"/>
+    <Plus class="size-12 fill-high"/>
   </button>
 </template>
 <script setup>
